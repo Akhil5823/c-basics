af5110158f2bc5d11311addb4be9f44df7cca904
Akhil5823 <78320904+Akhil5823@users.noreply.github.com>
2021-03-08 21:30:59 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+
+
+int max(int num1,int num2,int num3)
 {
-    printf("Hello world!\n");
+    int result;
+    if(num1>=num2 && num1>=num3)
+    {
+        result = num1;
+    }
+    else if(num2>=num1 && num2>=num3)
+    {
+        result = num2;
+
+    }
+    else{
+        result = num3;
+    }
+    return result;
+
+}
+
+int main()
+{   int x;
+    int y;
+    int z;
+    printf("first number: ");
+    scanf("%d",&x);
+    printf("second number: ");
+    scanf("%d",&y);
+    printf("third number: ");
+    scanf("%d",& z);
+
+    printf("greatest number is %d",max(x,y,z));
     return 0;
 }
